10a432c1eb2e67728c5ebddf54e0f5d817d2ff70
RamniwasMahala007 <71785474+RamniwasMahala007@users.noreply.github.com>
2023-01-18 18:37:32 +0530

    adding animation component

---
+++ src/Animation.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+function Animation()
+{
+    return(
+        <>
+            <div className='heading_style'>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 text-center">
+          <h3 class="animate-charcter"> These are the best web series of Ramniwas</h3>
+        </div>
+      </div>
+    </div>
+ </div>
+        </>
+    );
+
+}
+export default Animation;
--- src/index.js
+++ src/index.js
@@ -2,19 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Card from './Card';
+import Animation from './Animation';
 
 const root = document.getElementById('root');
 ReactDOM.render(<>
 
- <div className='heading_style'>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12 text-center">
-          <h3 class="animate-charcter"> These are the best web series of Ramniwas</h3>
-        </div>
-      </div>
-    </div>
- </div>
+<Animation/>
+ 
 <Card   sname ="The Witcher"
         imgsrc = "https://wallpapercave.com/w/wp4423069.jpg"
         title = "A Netflix Original Series"
