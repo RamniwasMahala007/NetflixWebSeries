63fe392dd8bbd72b2ebb7d39019aac91c5c19c80
RamniwasMahala007 <71785474+RamniwasMahala007@users.noreply.github.com>
2023-01-21 08:46:54 +0530

    adding SData file for using Data of movies using array

---
+++ src/SData.jsx
@@ -0,0 +1,27 @@
+const SData = [
+    {
+        sname:"The Witcher",
+        imgsrc:"https://wallpapercave.com/w/wp4423069.jpg",
+        title:"A Netflix Original Series",
+        link:"https://www.netflrc.com/in/title/80189685?source=35",
+    },
+    {
+        sname:"Shadow and Bone",
+        imgsrc:"https://wallpapercave.com/w/wp8781073.jpg",
+        title:"A Netflix Original Series",
+        link:"https://www.netflix.com/in/title/80236319?source=35",
+    },
+    {
+        sname:"Sweet Tooth",
+        imgsrc:"https://wallpapercave.com/w/wp9313468.jpg",
+        title:"A Netflix Original Series",
+        link:"https://www.netflix.com/in/title/81221380?source=35",
+    },
+    {
+        sname:"1899",
+        imgsrc:"https://wallpapercave.com/w/wp11766167.jpg",
+        title:"A Netflix Original Series",
+        link:"https://www.netflix.com/in/title/80214497?source=35",
+    }
+]
+export default SData;
